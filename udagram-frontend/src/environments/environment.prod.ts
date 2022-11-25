c807e89c69ccd9e6108f31b076aab77d2b3efe4c
OnuecheghekwuAdaugoSandra <Onuecheghekwuadaugo@gmail.com>
2022-11-25 21:24:52 +0100

    tenth commit

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ab7906cfc822546f3bc1955286800604-1667507032.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://acccfd8945a6b46b0a34338fafe09967-334804275.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
